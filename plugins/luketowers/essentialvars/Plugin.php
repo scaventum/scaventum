2f544b942fc8398167fe7993d05dca02e58709e7
Ryan Dreamconfig <ryan@dreamconfig.com>
2019-05-25 21:52:16 +1200

    Add dynamic section blocks

--- plugins/luketowers/essentialvars/Plugin.php
+++ plugins/luketowers/essentialvars/Plugin.php
@@ -1,5 +1,6 @@
 <?php namespace LukeTowers\EssentialVars;
 
+use App;
 use Lang;
 use View;
 use Event;
@@ -21,7 +22,7 @@ class Plugin extends PluginBase
     {
         return [
             'name'        => 'EssentialVars',
-            'description' => 'Adds the app_[url|logo|name|debug|description] variables to Mail & CMS templates',
+            'description' => 'Adds the app_[url|logo|favicon|name|debug|description] variables to Mail & CMS templates',
             'author'      => 'Luke Towers',
             'icon'        => 'icon-code',
         ];
@@ -32,30 +33,47 @@ class Plugin extends PluginBase
      *
      * @return array
      */
-     public function boot()
-     {
-         $appVars = [
-             'url'         => url('/'),
-             'logo'        => BrandSetting::getLogo() ?: url('/modules/backend/assets/images/october-logo.svg'),
-             'name'        => BrandSetting::get('app_name'),
-             'debug'       => Config::get('app.debug', false),
-             'description' => BrandSetting::get('app_tagline'),
-         ];
+    public function boot()
+    {
+        App::before(function () {
+            // Share the variables with the mail template system
+            Event::listen('mailer.beforeAddContent', function () {
+                $appVars = [
+                    'url'         => url('/'),
+                    'logo'        => BrandSetting::getLogo() ?: url('/modules/backend/assets/images/october-logo.svg'),
+                    'favicon'     => BrandSetting::getFavicon() ?: url('/modules/backend/assets/images/favicon.png'),
+                    'name'        => BrandSetting::get('app_name'),
+                    'debug'       => Config::get('app.debug', false),
+                    'description' => BrandSetting::get('app_tagline'),
+                ];
 
-         // Share the variables with the mail template system
-         View::share('app_url', $appVars['url']);
-         View::share('app_logo', $appVars['logo']);
-         View::share('app_name', $appVars['name']);
-         View::share('app_debug', $appVars['debug']);
-         View::share('app_description', $appVars['description']);
+                View::share('app_url', $appVars['url']);
+                View::share('app_logo', $appVars['logo']);
+                View::share('app_favicon', $appVars['favicon']);
+                View::share('app_name', $appVars['name']);
+                View::share('app_debug', $appVars['debug']);
+                View::share('app_description', $appVars['description']);
+            });
 
-         // Share the variables with the CMS template system
-         Event::listen('cms.page.beforeDisplay', function ($controller, $url, $page) use ($appVars) {
-             $controller->vars['app_url']         = $appVars['url'];
-             $controller->vars['app_logo']        = $appVars['logo'];
-             $controller->vars['app_name']        = $appVars['name'];
-             $controller->vars['app_debug']       = $appVars['debug'];
-             $controller->vars['app_description'] = $appVars['description'];
-         });
-     }
+
+            // Share the variables with the CMS template system
+            Event::listen('cms.page.beforeDisplay', function ($controller, $url, $page) {
+                $appVars = [
+                    'url'         => url('/'),
+                    'logo'        => BrandSetting::getLogo() ?: url('/modules/backend/assets/images/october-logo.svg'),
+                    'favicon'     => BrandSetting::getFavicon() ?: url('/modules/backend/assets/images/favicon.png'),
+                    'name'        => BrandSetting::get('app_name'),
+                    'debug'       => Config::get('app.debug', false),
+                    'description' => BrandSetting::get('app_tagline'),
+                ];
+
+                $controller->vars['app_url']         = $appVars['url'];
+                $controller->vars['app_logo']        = $appVars['logo'];
+                $controller->vars['app_favicon']     = $appVars['favicon'];
+                $controller->vars['app_name']        = $appVars['name'];
+                $controller->vars['app_debug']       = $appVars['debug'];
+                $controller->vars['app_description'] = $appVars['description'];
+            });
+        });
+    }
 }
